a0cdb63b91902a9dbf06df91ebc6ab2b4416e437
AlioshaRabeshko <Leflylll@gmail.com>
2020-08-28 15:39:33 +0300

    node updater

---
+++ updater/index.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const fs = require('fs');
+const http = require('http');
+const admzip = require('adm-zip');
+const rimraf = require('rimraf');
+const os = require('os');
+
+const FILES = ['diagnostics.db', 'patients.db', 'settings.db', 'installed.txt'];
+const osData =
+	os.platform === 'win32'
+		? {
+				download:
+					'http://s3.eu-central-1.amazonaws.com/udapp.bucket/win-unpacked.zip',
+				folder: 'win-unpacked',
+				exe: 'udapp.exe',
+		  }
+		: {
+				download:
+					'http://s3.eu-central-1.amazonaws.com/udapp.bucket/linux-unpacked.zip',
+				folder: 'linux-unpacked',
+				exe: 'udapp',
+		  };
+
+if (fs.existsSync('./udapp')) {
+	FILES.forEach((el) => {
+		const inputFile = `./udapp/${el}`;
+		if (fs.existsSync(inputFile)) fs.copyFileSync(inputFile, `./${el}`);
+	});
+	rimraf.sync('./udapp');
+}
+const file = fs.createWriteStream('udapp.zip');
+const request = http.get(osData.download, (response) => response.pipe(file));
+file.on('close', () => {
+	const zip = new admzip('udapp.zip', true);
+	zip.extractAllTo('./');
+	fs.unlinkSync('./udapp.zip');
+	fs.renameSync(osData.folder, `./udapp`);
+	if (os.platform !== 'win32') fs.chmodSync(`./udapp/${osData.exe}`, '777');
+	FILES.forEach((el) => {
+		fs.copyFileSync(`./${el}`, `./udapp/${el}`);
+		fs.unlinkSync(`./${el}`);
+	});
+});
